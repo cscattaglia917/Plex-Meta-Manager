5f06327a83205a366f3f4ba5e40b97ad245e2224
cscattaglia917 <cscattaglia@gmail.com>
2022-05-17 13:28:22 +0000

    Cleanup/config changes

--- docker-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t pmm .
-docker stop pmm
-docker rm pmm
--- docker-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker stop pmm
-docker rm pmm
-docker run \
-  --name=pmm \
-  -v $PWD:/app \
-  -v $PWD/config:/config \
-  pmm -r --debug --trace --collections-only --run-collections "Captain America" 
